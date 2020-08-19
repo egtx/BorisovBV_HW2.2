1ee98812b6f8e7ead5baefb00954566b0b14f69c
egtx <eighthjune@mail.ru>
2020-08-20 01:03:54 +0300

    make outlets and action

---
+++ BorisovBV_HW2.2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  BorisovBV_HW2.2
+//
+//  Created by Борис  on 20.08.2020.
+//  Copyright © 2020 EightSixStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var redLight: UIView!
+    
+    @IBOutlet weak var yellowLight: UIView!
+    
+    @IBOutlet weak var greenLight: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func pressedButton(_ sender: UIButton) {
+    }
+}
+
