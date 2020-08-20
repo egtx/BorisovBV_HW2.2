29ceb11c89a7785b347117742eaf01fc80665349
egtx <eighthjune@mail.ru>
2020-08-20 04:30:50 +0300

    End

--- BorisovBV_HW2.2/ViewController.swift
+++ BorisovBV_HW2.2/ViewController.swift
@@ -16,13 +16,55 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var greenLight: UIView!
     
+
+    @IBOutlet weak var buttonLabel: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        redLight.layer.cornerRadius = redLight.frame.size.width/2
+        redLight.clipsToBounds = true
+        redLight.alpha = 0.3
+           
+        yellowLight.layer.cornerRadius = yellowLight.frame.size.width/2
+        yellowLight.clipsToBounds = true
+        yellowLight.alpha = 0.3
+        
+        greenLight.layer.cornerRadius = greenLight.frame.size.width/2
+        greenLight.clipsToBounds = true
+        greenLight.alpha = 0.3
+        
+        
+        
     }
-
-
+    
+var i = 1
+    
     @IBAction func pressedButton(_ sender: UIButton) {
+        
+      
+        buttonLabel.setTitle("Next", for: .normal)
+        
+   
+        
+        if i==3{
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            i = 1
+        }
+        else if i==2 {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            i = 3
+        }else if i==1{
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            i = 2
+        }
+        
+            
+      
+        
     }
 }
 
